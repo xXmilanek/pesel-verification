4fe22e96d736d63678f9fd2598cea7a0dd717e07
xXmilanek <43542037+xXmilanek@users.noreply.github.com>
2019-07-29 20:02:21 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,28 @@ window.addEventListener('load', () => {
 		for (const pair of formData.entries()) {
 			const pesel = pair[1];
 
-			// Do something
+			var array = pesel.toString().split('').map((item, index) => parseInt(item));
+			const a = array[0];
+			const b = array[1];
+			const c = array[2];
+			const d = array[3];
+			const e = array[4];
+			const f = array[5];
+			const g = array[6];
+			const h = array[7];
+			const i = array[8];
+			const j = array[9];
+			const k = array[10];
 
-			alert(`Podany pesel to: ${pesel}`);
+			var l = (9*a + 7*b + 3*c + 1*d + 9*e + 7*f + 3*g + 1*h + 9*i + 7*j) % 10;
+
+			if (k === l) {
+				alert(`pesel jest poprawny`);
+			}
+			else {
+				alert(`pesel jest niepoprawny`)
+			}
+
 		}
 	};
 
