cb32790e5e06755d6ff2e38ac4f3479ca5de0bf6
xXmilanek <43542037+xXmilanek@users.noreply.github.com>
2019-07-31 19:59:58 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,13 +21,69 @@ window.addEventListener('load', () => {
 			const k = array[10];
 
 			var l = (9*a + 7*b + 3*c + 1*d + 9*e + 7*f + 3*g + 1*h + 9*i + 7*j) % 10;
+			const m = c.toString() + d.toString();
+			Number(m);
 
 			if (k === l) {
-				alert(`pesel jest poprawny`);
+
+				info1.textContent = 'PESEL jest prawidłowy';
+
+				if  ((m >=1)&&(m <=12)){
+
+						info2.textContent = `Data urodzenia to: 19${a}${b}.${m}.${e}${f}`;
+
+				}
+
+				else if  ((m >=21)&&(m <=32)){
+					const m2 = m-20;
+
+					if (m2 < 10){
+						info2.textContent = `Data urodzenia to: 20${a}${b}.0${m2}.${e}${f}`;
+					}
+					else {
+						info2.textContent = `Data urodzenia to: 20${a}${b}.${m2}.${e}${f}`;
+					}
+				}
+				else if  ((m >=41)&&(m <=52)){
+					const m2 = m-40;
+
+					if (m2 < 10){
+						info2.textContent = `Data urodzenia to: 21${a}${b}.0${m2}.${e}${f}`;
+					}
+					else {
+						info2.textContent = `Data urodzenia to: 21${a}${b}.${m2}.${e}${f}`;
+					}
+				}
+				else if  ((m >=61)&&(m <=72)){
+					const m2 = m-60;
+
+					if (m2 < 10){
+						info2.textContent = `Data urodzenia to: 22${a}${b}.0${m2}.${e}${f}`;
+					}
+					else {
+						info2.textContent = `Data urodzenia to: 22${a}${b}.${m2}.${e}${f}`;
+					}
+				}
+				if (j%2==0){
+					info3.textContent = `Płeć: kobieta`;
+				}
+				else {
+					info3.textContent = `Płeć: Mężczyzna`;
+				}
+
+				info4.textContent = `Numer seryjny: ${g}${h}${i}${j}`;
+
 			}
 			else {
-				alert(`pesel jest niepoprawny`)
+				info1.textContent = 'PESEL nie jest prawidłowy';
+				info2.textContent = ``;
+				info3.textContent = ``;
+				info4.textContent = ``;
+
 			}
+
+
+
 
 		}
 	};
